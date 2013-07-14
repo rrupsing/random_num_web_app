80fd5f7bda97091a3bc73d27350b88b78d2c5a89
Raul Rupsingh <raul@athinkingape.com>
2013-07-14 11:27:23 -0400

    cleanup

--- django_server/django_server/urls.py
+++ django_server/django_server/urls.py
@@ -1,20 +1,9 @@
 from django.conf.urls import patterns, include, url
-"""
-from rest_framework import viewsets, routers
 
-# ViewSets define the view behavior.
-class RandomNumberProfileViewSet(viewsets.ModelViewSet):
-    model = RandomNumberProfile
-
-# Routers provide an easy way of automatically determining the URL conf
-router = routers.DefaultRouter()
-router.register(r'random_number_profiles', RandomNumberProfileViewSet)
-"""
 urlpatterns = patterns('',
     # Examples:
     #    url(r'^', include(router.urls)),
     url(r'^number_profile', 'django_server.views.number_profile'),
     url(r'^generate_random', 'django_server.views.generate_random'),
-    url(r'^room/?', include('django_server.room.urls')),
 
 )
