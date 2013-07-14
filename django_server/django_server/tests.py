014e8830000e20a9e8ee693c6133890ec15967cf
Raul Rupsingh <raul@athinkingape.com>
2013-07-14 12:03:24 -0400

    adding test of clean

--- django_server/django_server/tests.py
+++ django_server/django_server/tests.py
@@ -2,7 +2,9 @@ import unittest
 from models import *
 import config as config
 import time
-from room.tests import *
+from tasks import clean
+from django.utils import timezone
+import datetime
 
 class RandomNumberManagerTestCase(unittest.TestCase):
     """
@@ -12,4 +14,13 @@ class RandomNumberManagerTestCase(unittest.TestCase):
         pass
 
     def tearDown(self):
-        pass
+        RandomNumberProfile.objects.all().delete()
+
+    def testCleanup(self):
+        RandomNumberProfile.objects.create(range_start=10, range_end=25, create_date=timezone.now()-datetime.timedelta(days=2))
+        clean()
+
+        self.assertEquals(RandomNumberProfile.objects.all().count(), 0)
+
+
+
