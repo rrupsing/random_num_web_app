0b9a22a4d05d0a1215824360d566a960d75259cc
Raul Rupsingh <raul@athinkingape.com>
2013-07-14 11:48:05 -0400

    better error handling

--- django_server/django_server/views.py
+++ django_server/django_server/views.py
@@ -10,13 +10,13 @@ def generate_random(request):
     range_end = None
     seed = None
 
-    if request.GET.has_key('random_number_profile_id'):
+    if request.GET.has_key('random_number_profile_id') and isinstance(request.GET.has_key('random_number_profile_id'), int):
         random_number_profile_id = int(request.GET['random_number_profile_id'])
-    if request.GET.has_key('range_start'):
+    if request.GET.has_key('range_start') and isinstance(request.GET.has_key('range_start'), int):
         range_start = int(request.GET['range_start'])
-    if request.GET.has_key('range_end'):
+    if request.GET.has_key('range_end') and isinstance(request.GET.has_key('range_end'), int):
         range_end = int(request.GET['range_end'])
-    if request.GET.has_key('seed'):
+    if request.GET.has_key('seed') and isinstance(request.GET.has_key('seed'), int):
         seed = int(request.GET['seed'])
 
     if random_number_profile_id is None and (range_start is None or range_end is None):
@@ -35,13 +35,13 @@ def number_profile(request):
     range_end = None
     seed = None
 
-    if request.POST.has_key('random_number_profile_id'):
+    if request.POST.has_key('random_number_profile_id') and isinstance(request.GET.has_key('random_number_profile_id'), int):
         random_number_profile_id = int(request.POST['random_number_profile_id'])
-    if request.POST.has_key('range_start'):
+    if request.POST.has_key('range_start') and isinstance(request.GET.has_key('range_start'), int):
         range_start = int(request.POST['range_start'])
-    if request.POST.has_key('range_end'):
+    if request.POST.has_key('range_end') and isinstance(request.GET.has_key('range_end'), int):
         range_end = int(request.POST['range_end'])
-    if request.POST.has_key('seed'):
+    if request.POST.has_key('seed') and isinstance(request.GET.has_key('seed'), int):
         seed = int(request.POST['seed'])
 
     # proper way to do this is to detect the HTTP DELETE command and process, however I didn't have time to integrate the django rest library
